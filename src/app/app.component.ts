fae22b64df95f8f7d87d2b2bfc625f7b63789e31
SimonFranssonSF <fransson.simon@outlook.com>
2017-09-22 19:59:35 +0200

    Started the tutorial, components, one-way bindings, two-way bindings, templates, properties, styles, import FormModule to app.module.ts, Mock objects are the data atm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
+export class Player {
+  id: number;
+  name: string;
+  level: number;
+}
+
+// Mock objects
+const PLAYERS: Player[] = [
+  { id: 11, name: 'Mr. Nice', level: 500 },
+  { id: 12, name: 'Narco', level: 500 },
+  { id: 13, name: 'Bombasto', level: 500 },
+  { id: 14, name: 'Celeritas', level: 500 },
+  { id: 15, name: 'Magneta', level: 500 },
+  { id: 16, name: 'RubberMan', level: 500 },
+  { id: 17, name: 'Dynama', level: 500 },
+  { id: 18, name: 'Dr IQ', level: 500 },
+  { id: 19, name: 'Magma', level: 500 },
+  { id: 20, name: 'Tornado', level: 500 }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Simon\'s first Angular 2 application';
+  title = 'Simon\'s player app';
+  players = PLAYERS;
+  selectedPlayer: Player;
+
+  onSelect(player: Player): void {
+    this.selectedPlayer = player;
+  }
 }
+
+
