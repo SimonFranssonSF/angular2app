78460bbd0bf8c835b38badfdbaac3f30aeb34cc2
SimonFranssonSF <fransson.simon@outlook.com>
2017-09-24 03:48:48 +0200

    Added a service which gets the mock object PLAYERS and initlizes them as players Player[], the getPlayer() is called in OnInit which is run when app.component.ts is active, a promise is added to the getPlayers() method to deal with asynchronious calls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Player } from './player';
-
-// Mock objects
-const PLAYERS: Player[] = [
-  { id: 11, name: 'Mr. Nice', level: 500 },
-  { id: 12, name: 'Narco', level: 500 },
-  { id: 13, name: 'Bombasto', level: 500 },
-  { id: 14, name: 'Celeritas', level: 500 },
-  { id: 15, name: 'Magneta', level: 500 },
-  { id: 16, name: 'RubberMan', level: 500 },
-  { id: 17, name: 'Dynama', level: 500 },
-  { id: 18, name: 'Dr IQ', level: 500 },
-  { id: 19, name: 'Magma', level: 500 },
-  { id: 20, name: 'Tornado', level: 500 }
-];
+import { PlayerService } from './player.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [PlayerService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Simon\'s player app';
-  players = PLAYERS;
+  players: Player[];
   selectedPlayer: Player;
 
+  constructor(private playerService: PlayerService) { }
+
+  getPlayers(): void {
+    this.playerService.getPlayers().then(players => this.players = players);   
+  }
+
+  ngOnInit(): void {
+    this.getPlayers();
+  }
+  
   onSelect(player: Player): void {
     this.selectedPlayer = player;
   }
---
+++ src/app/mock-players.ts
@@ -0,0 +1,15 @@
+import { Player } from './player';
+
+// Mock objects
+export const PLAYERS: Player[] = [
+    { id: 11, name: 'Mr. Nice', level: 500 },
+    { id: 12, name: 'Narco', level: 500 },
+    { id: 13, name: 'Bombasto', level: 500 },
+    { id: 14, name: 'Celeritas', level: 500 },
+    { id: 15, name: 'Magneta', level: 500 },
+    { id: 16, name: 'RubberMan', level: 500 },
+    { id: 17, name: 'Dynama', level: 500 },
+    { id: 18, name: 'Dr IQ', level: 500 },
+    { id: 19, name: 'Magma', level: 500 },
+    { id: 20, name: 'Tornado', level: 500 }
+  ];
